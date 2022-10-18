1803d851b2e455c1ae2e5dda512b1e0b9263a699
Thibault Lestang <thibault.lestang@protonmail.com>
2022-10-18 13:38:02 +0100

    thomas function takes array of coefficients

--- src/main_thomas.f90
+++ src/main_thomas.f90
@@ -3,10 +3,11 @@ program main
   implicit none
 
   real, parameter :: alpha = 2.
-  real, parameter :: d(3) = [1, 2, 3]
+  real, parameter :: ds(3) = [1, 2, 3]
+  real, parameter :: bs(3) = [1, 1, 1]
   real :: x(3)
 
-  x = thomas(alpha, d)
+  x = thomas(bs, ds, alpha)
 
   ! Expect x = - 5. / 7.
   !        y =   6. / 7.
--- src/module_thomas.f90
+++ src/module_thomas.f90
@@ -3,28 +3,26 @@ module thomas_module
 
 contains
 
-  pure function thomas(alpha, d) result(x)
+  pure function thomas(bs, ds, alpha) result(x)
     real, intent(in) :: alpha
-    real, intent(in) :: d(:)
-    real, allocatable :: b(:), x(:), dd(:)
+    real, intent(in) :: bs(:), ds(:)
+    real, allocatable :: x(:), b(:), d(:)
     integer :: i, n
     real :: w
 
-    allocate(dd, source=d)
-    allocate(b, source=d)
-    allocate(c, source=d)
-    allocate(x, source=d)
-    n = size(d)
-    b = 1.
+    allocate(d, source=ds)
+    allocate(b, source=bs)
+    allocate(x, source=ds)
+    n = size(ds)
     do i = 2, n
        w = alpha / b(i-1)
-       b(i) = 1. - w * alpha
-       dd(i) = dd(i) - w * dd(i-1)
+       b(i) = b(i) - w * alpha
+       d(i) = d(i) - w * d(i-1)
     end do
 
-    x(n) = dd(n) / b(n)
+    x(n) = d(n) / b(n)
     do i = n-1, 1, -1
-       x(i) = (dd(i) - alpha * x(i+1)) / b(i)
+       x(i) = (d(i) - alpha * x(i+1)) / b(i)
     end do
   end function thomas
 end module thomas_module
