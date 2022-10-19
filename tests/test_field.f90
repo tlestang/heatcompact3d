4a290d9503ac9757174eabd773ab0e089475e2ec
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-10-19 12:36:04 +0100

    Report on overall pass/fail for test_field

--- tests/test_field.f90
+++ tests/test_field.f90
@@ -23,19 +23,21 @@ program test_field
         end do
      end do
   end do
-
   temp_field = Field(u0)
+
+  allpass = .true.
 
   if (.not. all([nx, ny, nz] == &
        &[temp_field%nx(), temp_field%ny(), temp_field%nz()])) then
      write(stderr, '(a)') 'Field data has the correct rank and size... failed.'
-
+     allpass = .false.
   else
      write(stderr, '(a)') 'Field data has the correct rank and size... passed.'
   end if
 
   if(.not. temp_field%is_equal(Field(u0), tol)) then
      write(stderr, '(a)') 'Field equal operator... failed.'
+     allpass = .false.
   else
      write(stderr, '(a)') 'Field equal operator... passed.'
   end if
@@ -44,7 +46,14 @@ program test_field
   rhs = temp_field%rhs(dx)
   if(.not. expected%is_equal(rhs, tol)) then
      write(stderr, '(a)') 'Field right hand side is computed correctly... failed.'
+     allpass = .false.
   else
      write(stderr, '(a)') 'Field right hand side is computed correctly... passed.'
+  end if
+
+  if (allpass) then
+     write(stderr, '(a)') 'All tests passed successfully'
+  else
+     write(stderr, '(a)') '!!! Some tests failed !!!'
   end if
 end program test_field
