b35ad06532a291b9edd24133def412cbc1afc445
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-10-24 08:09:19 +0100

    Add failing test for integration module

---
+++ tests/test_time_integration.f90
@@ -0,0 +1,50 @@
+program test_field
+  use iso_fortran_env, only: stderr => error_unit
+  use field_module, only: Field
+  use time_integration, only: euler_integrator_type
+  implicit none
+
+  real :: u0(16, 16, 16)
+  type(Field) :: temp_field, expected
+  real, parameter :: tol = 0.1
+  integer :: i, j, k, nx, ny, nz
+  logical :: allpass
+  real :: dx
+  type(euler_integrator_type) :: euler
+
+  nx = size(u0, 1)
+  ny = size(u0, 2)
+  nz = size(u0, 3)
+  dx = 2. * acos(-1.) / (nx - 1)
+
+  do k = 1, nz
+     do j = 1, ny
+        do i = 1, nx
+           u0(i, j, k) = &
+                & cos((i-1)*dx) * cos((j-1)*dx) * &
+                & cos((k-1)*dx)
+        end do
+     end do
+  end do
+
+  allpass = .true.
+
+  temp_field = Field(u0, dx)
+  euler = euler_integrator_type(starttime=0., endtime=0.5, dt=0.1)
+  call euler%integrate(temp_field)
+  expected = Field( &
+       & (1. - 3. * self%a * self%dt) ** 5 * u0)
+  if (.not. expected%is_equal(temp_field, tol)) then
+     write(stderr, '(a)') 'Foward integration is computed correctly... failed.'
+     allpass = .false.
+  else
+     write(stderr, '(a)') 'Foward integration is computed correctly... passed.'
+  end if
+
+  if (allpass) then
+     write(stderr, '(a)') 'All tests passed successfully'
+  else
+     write(stderr, '(a)') '!!! Some tests failed !!!'
+  end if
+
+end program test_field
