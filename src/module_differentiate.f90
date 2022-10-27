5378d30f83837389ebd944e84557f694af5502e1
Thibault Lestang <thibault.lestang@protonmail.com>
2022-10-27 14:56:29 +0100

    (Differentiate) Module wide renamings

--- src/module_differentiate.f90
+++ src/module_differentiate.f90
@@ -2,16 +2,16 @@ module differentiate
   use thomas_module, only: thomas
   implicit none
 
-  integer, parameter :: dirichlet_stencils(8) = [ &
+  integer, parameter :: dirichlet_nodes(8) = [ &
        & 1, 2, 3, 4, &
        & 1, 2, 3, 4 &
        & ]
 
   type :: differentiator_type
      private
-     real :: east_coeffs(4, 2)
-     real :: east_coeffs2(4, 2)
-     integer :: east_stencils(4, 2)
+     real :: coeffs(4, 2)
+     real :: coeffs2(4, 2)
+     integer :: nodes(4, 2)
    contains
      procedure, public :: diff, diff2
   end type differentiator_type
@@ -24,9 +24,9 @@ contains
     type(differentiator_type) :: dirichlet_differentiator
 
     dirichlet_differentiator = differentiator_type( &
-         & east_coeffs = reshape(dirichlet_coeffs, [4, 2]), &
-         & east_coeffs2 = reshape(dirichlet_2_coeffs, [4, 2]), &
-         & east_stencils = reshape(dirichlet_stencils, [4, 2]) &
+         & coeffs = reshape(dirichlet_coeffs, [4, 2]), &
+         & coeffs2 = reshape(dirichlet_2_coeffs, [4, 2]), &
+         & nodes = reshape(dirichlet_nodes, [4, 2]) &
          & )
   end function dirichlet_differentiator
 
@@ -35,9 +35,9 @@ contains
     type(differentiator_type) :: neumann_odd_differentiator
 
     neumann_odd_differentiator = differentiator_type( &
-         & east_coeffs = reshape(neumann_odd_coeffs, [4, 2]), &
-         & east_coeffs2 = reshape(neumann_odd_coeffs, [4, 2]), &
-         & east_stencils = reshape(dirichlet_stencils, [4, 2]) &
+         & coeffs = reshape(neumann_odd_coeffs, [4, 2]), &
+         & coeffs2 = reshape(neumann_odd_coeffs, [4, 2]), &
+         & nodes = reshape(dirichlet_nodes, [4, 2]) &
          & )
   end function neumann_odd_differentiator
 
@@ -47,24 +47,26 @@ contains
     real, intent(in) :: f(:)
     real, intent(in) :: dx
     real, allocatable :: df(:), rhs(:), diag(:), as(:), cs(:)
-    integer :: west_stencils(4, 2)
+    integer :: west_nodes(4, 2)
     integer :: nx, i
-    real :: w(4)
+    real :: bulk_coeffs(4), east_coeffs(4, 2), west_coeffs(4, 2)
 
     allocate(rhs, source=f)
 
-    rhs(1) = dot_product(f(self%east_stencils(:, 1)), self%east_coeffs(:, 1) / dx)
-    rhs(2) = dot_product(f(self%east_stencils(:, 2)), self%east_coeffs(:, 2) / dx)
+    east_coeffs = self%coeffs / dx
+    rhs(1) = dot_product(f(self%nodes(:, 1)), east_coeffs(:, 1))
+    rhs(2) = dot_product(f(self%nodes(:, 2)), east_coeffs(:, 2))
 
     nx = size(f)
-    w = sixth_order_compact_coeffs / dx
+    bulk_coeffs = sixth_order_compact_coeffs / dx
     do i=3,nx-2
-       rhs(i) = dot_product(f([i-2, i-1, i+1, i+2]), w)
+       rhs(i) = dot_product(f([i-2, i-1, i+1, i+2]), bulk_coeffs)
     end do
 
-    west_stencils = nx - self%east_stencils + 1
-    rhs(nx - 1) = dot_product(f(west_stencils(:, 2)), - self%east_coeffs(:, 2) / dx)
-    rhs(nx) = dot_product(f(west_stencils(:, 1)), - self%east_coeffs(:, 1) / dx)
+    west_nodes = nx - self%nodes + 1
+    west_coeffs = -1. * self%coeffs / dx
+    rhs(nx - 1) = dot_product(f(west_nodes(:, 2)), west_coeffs(:, 2))
+    rhs(nx) = dot_product(f(west_nodes(:, 1)), west_coeffs(:, 1))
 
     diag = [(1., i=1, nx)]
     cs = [2., 1. / 4., (alpha_coeffs, i=3, nx-2), 1. / 4., 0.]
@@ -78,24 +80,26 @@ contains
     real, intent(in) :: f(:)
     real, intent(in) :: dx
     real, allocatable :: ddf(:), rhs(:), diag(:), as(:), cs(:)
-    integer :: west_stencils(4, 2)
+    integer :: west_nodes(4, 2)
     integer :: nx, i
-    real :: w(5)
+    real :: bulk_coeffs(5), east_coeffs(4, 2), west_coeffs(4, 2)
 
     allocate(rhs, source=f)
 
-    rhs(1) = dot_product(f(self%east_stencils(:, 1)), self%east_coeffs2(:, 1) / dx / dx)
-    rhs(2) = dot_product(f(self%east_stencils(:, 2)), self%east_coeffs2(:, 2) / dx / dx)
+    east_coeffs = self%coeffs2 / dx / dx
+    rhs(1) = dot_product(f(self%nodes(:, 1)), east_coeffs(:, 1))
+    rhs(2) = dot_product(f(self%nodes(:, 2)), east_coeffs(:, 2))
 
     nx = size(f)
-    w = sixth_order_compact_2_coeffs / dx / dx
+    bulk_coeffs = sixth_order_compact_2_coeffs / dx / dx
     do i=3,nx-2
-       rhs(i) = dot_product(f([i-2, i-1, i, i+1, i+2]), w)
+       rhs(i) = dot_product(f([i-2, i-1, i, i+1, i+2]), bulk_coeffs)
     end do
 
-    west_stencils = nx - self%east_stencils + 1
-    rhs(nx - 1) = dot_product(f(west_stencils(:, 2)), self%east_coeffs2(:, 2) / dx / dx)
-    rhs(nx) = dot_product(f(west_stencils(:, 1)), self%east_coeffs2(:, 1) / dx / dx)
+    west_nodes = nx - self%nodes + 1
+    west_coeffs = + self%coeffs2 / dx / dx
+    rhs(nx - 1) = dot_product(f(west_nodes(:, 2)), west_coeffs(:, 2))
+    rhs(nx) = dot_product(f(west_nodes(:, 1)), west_coeffs(:, 1))
 
     diag = [(1., i=1, nx)]
     cs = [11., 1. / 10., (alpha_2_coeffs, i=3, nx-2), 1. / 10., 0.]
