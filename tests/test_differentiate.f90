695f381d4ffa2ffc069a98e4dff21d25a40ff023
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-10-29 16:54:26 +0100

    Test first derivative with stencil-based implementation

--- tests/test_differentiate.f90
+++ tests/test_differentiate.f90
@@ -1,17 +1,19 @@
 program test_differentiate
   use iso_fortran_env, only: stderr => error_unit
   use differentiate, only: differentiator_type, &
-       & dirichlet_differentiator, neumann_odd_differentiator, diff2
+       & sixth_order_compact
+  use boundary_schemes, only: boundary_type, get_dirichlet_boundary
   implicit none
 
-  real :: f(20), df(20), expected(20)
+  real, allocatable :: f(:), df(:), expected(:)
   real, parameter :: tol = 0.1
   type(differentiator_type) :: differentiator
+  type(boundary_type) :: dirichlet
   integer :: i, n
   logical :: allpass
   real :: dx
 
-  n = size(f)
+  n = 20
   dx = 2. * acos(-1.) / (n - 1)
   f = [(sin((i-1)*dx), i=1,n)]
 
@@ -19,7 +21,11 @@ program test_differentiate
 
   ! First derivative with dirichlet both ends
   expected = [(cos((i-1)*dx), i=1,n)]
-  differentiator = dirichlet_differentiator()
+  dirichlet = get_dirichlet_boundary()
+  differentiator = sixth_order_compact( &
+       & east = dirichlet, &
+       & west = dirichlet &
+       & )
   df = differentiator%diff(f, dx)
   if (.not. all(abs(df - expected) < tol)) then
      allpass = .false.
@@ -28,28 +34,28 @@ program test_differentiate
      write(stderr, '(a)') 'First derivatives are computed correctly... passed'
   end if
 
-  ! Second derivative
-  expected = [(- sin((i-1)*dx), i=1, n)]
-  df = differentiator%diff2(f, dx)
-  if (.not. all(abs(df - expected) < tol)) then
-     allpass = .false.
-     write(stderr, '(a)') 'Second derivatives are computed correctly... failed'
-  else
-     write(stderr, '(a)') 'Second derivatives are computed correctly... passed'
-  end if
+  ! ! Second derivative
+  ! expected = [(- sin((i-1)*dx), i=1, n)]
+  ! df = differentiator%diff2(f, dx)
+  ! if (.not. all(abs(df - expected) < tol)) then
+  !    allpass = .false.
+  !    write(stderr, '(a)') 'Second derivatives are computed correctly... failed'
+  ! else
+  !    write(stderr, '(a)') 'Second derivatives are computed correctly... passed'
+  ! end if
 
-  dx = acos(-1.) / (n - 1)
-  f = [(cos((i-1)*dx), i=1,n)]
-  ! First derivative with neumann odd both ends
-  expected = [(-sin((i-1)*dx), i=1,n)]
-  differentiator = neumann_odd_differentiator()
-  df = differentiator%diff(f, dx)
-  if (.not. all(abs(df - expected) < tol)) then
-     allpass = .false.
-     write(stderr, '(a)') 'First derivatives are computed correctly... failed'
-  else
-     write(stderr, '(a)') 'First derivatives are computed correctly... passed'
-  end if
+  ! dx = acos(-1.) / (n - 1)
+  ! f = [(cos((i-1)*dx), i=1,n)]
+  ! ! First derivative with neumann odd both ends
+  ! expected = [(-sin((i-1)*dx), i=1,n)]
+  ! differentiator = neumann_odd_differentiator()
+  ! df = differentiator%diff(f, dx)
+  ! if (.not. all(abs(df - expected) < tol)) then
+  !    allpass = .false.
+  !    write(stderr, '(a)') 'First derivatives are computed correctly... failed'
+  ! else
+  !    write(stderr, '(a)') 'First derivatives are computed correctly... passed'
+  ! end if
 
   if (allpass) then
      write(stderr, '(a)') 'All tests passed successfully'
