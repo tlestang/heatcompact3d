334ff2e04cb41a558b59de4b95bea1aad5e93036
Thibault Lestang <thibault.lestang@protonmail.com>
2022-11-01 16:12:59 +0000

    Test periodic second derivatives

--- tests/test_differentiate.f90
+++ tests/test_differentiate.f90
@@ -1,7 +1,7 @@
 program test_differentiate
   use iso_fortran_env, only: stderr => error_unit
   use differentiate, only: differentiator_type, &
-       & sixth_order_compact
+       & sixth_order_compact, sixth_order_compact_2nd
   use boundary_schemes, only: boundary_type, get_dirichlet_boundary
   implicit none
 
@@ -30,6 +30,7 @@ program test_differentiate
      write(stderr, '(a)') 'First derivatives (periodic) are computed correctly... passed'
   end if
   deallocate(differentiator)
+
   ! First derivative with dirichlet both ends
   expected = [(cos((i-1)*dx), i=1,n)]
   dirichlet = get_dirichlet_boundary()
@@ -44,29 +45,19 @@ program test_differentiate
   else
      write(stderr, '(a)') 'First derivatives (Dirichlet) are computed correctly... passed'
   end if
+  deallocate(differentiator)
 
-  ! ! Second derivative
-  ! expected = [(- sin((i-1)*dx), i=1, n)]
-  ! df = differentiator%diff2(f, dx)
-  ! if (.not. all(abs(df - expected) < tol)) then
-  !    allpass = .false.
-  !    write(stderr, '(a)') 'Second derivatives are computed correctly... failed'
-  ! else
-  !    write(stderr, '(a)') 'Second derivatives are computed correctly... passed'
-  ! end if
-
-  ! dx = acos(-1.) / (n - 1)
-  ! f = [(cos((i-1)*dx), i=1,n)]
-  ! ! First derivative with neumann odd both ends
-  ! expected = [(-sin((i-1)*dx), i=1,n)]
-  ! differentiator = neumann_odd_differentiator()
-  ! df = differentiator%diff(f, dx)
-  ! if (.not. all(abs(df - expected) < tol)) then
-  !    allpass = .false.
-  !    write(stderr, '(a)') 'First derivatives are computed correctly... failed'
-  ! else
-  !    write(stderr, '(a)') 'First derivatives are computed correctly... passed'
-  ! end if
+  ! Second derivative with periodic boundary conditions
+  expected = [(- sin((i-1)*dx), i=1, n)]
+  differentiator = sixth_order_compact_2nd()
+  df = differentiator%diff(f, dx * dx)
+  if (.not. all(abs(df - expected) < tol)) then
+     allpass = .false.
+     write(stderr, '(a)') 'Second derivatives (periodic) are computed correctly... failed'
+  else
+     write(stderr, '(a)') 'Second derivatives (periodic) are computed correctly... passed'
+  end if
+  deallocate(differentiator)
 
   if (allpass) then
      write(stderr, '(a)') 'All tests passed successfully'
