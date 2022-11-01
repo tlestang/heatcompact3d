336667f9a1a2dbff45b5f93ca6b98ad529bb3653
Thibault Lestang <thibault.lestang@protonmail.com>
2022-11-01 16:01:06 +0000

    Not all stencil have the same size, define them in separate file

--- src/module_stencil.f90
+++ src/module_stencil.f90
@@ -1,8 +1,9 @@
 module stencil
   implicit none
   type :: stencil_type
-     integer :: nodes(4)
-     real :: coeffs(4), lower, upper
+     integer, allocatable :: nodes(:)
+     real, allocatable :: coeffs(:)
+     real :: lower, upper
    contains
      procedure, private :: stencil_mul_real
      procedure, public :: apply => apply_stencil
@@ -11,14 +12,6 @@ module stencil
      procedure, public :: flip, is_equal
      generic :: operator(*) => stencil_mul_real
   end type stencil_type
-
-  type(stencil_type), parameter :: sixth_order_compact_stencil = &
-       stencil_type( &
-       & nodes = [-2, -1, 1, 2], &
-       & coeffs = [- 1. / 36., - 7. / 9., + 7. / 9., + 1. / 36.], &
-       & upper = 1. / 3.,  &
-       & lower = 1. / 3. &
-       & )
 
 contains
 
@@ -95,43 +88,3 @@ contains
   end function apply_stencil_along
 
 end module stencil
-
-module boundary_schemes
-  use stencil, only: stencil_type
-  type boundary_type
-     type(stencil_type) :: first_order_east(2), second_order_east(2)
-     type(stencil_type) :: first_order_west(2), second_order_west(2)
-  end type boundary_type
-
-contains
-
-  type(boundary_type) function get_dirichlet_boundary()
-    type(stencil_type) :: first_order_east(2), second_order_east(2)
-    type(stencil_type) :: first_order_west(2), second_order_west(2)
-
-    first_order_east(1) = stencil_type( &
-         & nodes = [0, 1, 2, 3], &
-         & coeffs = [-5. / 2., 2., 0.5, 0.], &
-         & lower = 0., upper = 2. &
-         & )
-    first_order_east(2) = stencil_type( &
-         & nodes = [-1, 0, 1, 2], &
-         & coeffs = [-3. / 4., 0., 3. / 4., 0.], &
-         & lower = 1. / 4., upper = 1. / 4. &
-         & )
-    second_order_east(1) = stencil_type( &
-         & nodes = [0, 1, 2, 3], &
-         & coeffs = [13., -27., 15., -1.], &
-         & lower = 0., upper = 11. &
-         & )
-    second_order_east(2) = stencil_type( &
-         & nodes = [-1, 0, 1, 2], &
-         & coeffs = [6. / 5., -12. / 5., 6. / 5., 0.], &
-         & lower = 1. / 10., upper = 1. / 10. &
-         & )
-    first_order_west = first_order_east%flip() * (-1.)
-    second_order_west = second_order_east%flip()
-    get_dirichlet_boundary = boundary_type( &
-         & first_order_east, second_order_east, first_order_west, second_order_west)
-  end function get_dirichlet_boundary
-end module boundary_schemes
