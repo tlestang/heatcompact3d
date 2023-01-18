aacdcdb90b47393999c236eab911b9b70f433b19
Semih Akkurt <sa4018@kolmogorov.ae.ic.ac.uk>
2023-01-18 18:13:50 +0000

    GPU field test passes, haha!

--- src/module_field_gpu.f90
+++ src/module_field_gpu.f90
@@ -36,9 +36,20 @@ contains
        allocate(temp_dev, mold=self%data)
 
        rhs_dev = self%data
+       ! x-directional derivatives
        grid = dim3(ceiling(real(self%ny())/th), ceiling(real(self%nz())/th), 1)
        threads = dim3(th, th, 1)
        call rhs_x<<<grid, threads>>>(data_dev, rhs_dev, temp_dev)
+
+       ! y-directional derivatives
+       grid = dim3(ceiling(real(self%nx())/th), ceiling(real(self%nz())/th), 1)
+       threads = dim3(th, th, 1)
+       call rhs_y<<<grid, threads>>>(data_dev, rhs_dev, temp_dev)
+
+       ! z-directional derivatives
+       grid = dim3(ceiling(real(self%nx())/th), ceiling(real(self%ny())/th), 1)
+       threads = dim3(th, th, 1)
+       call rhs_z<<<grid, threads>>>(data_dev, rhs_dev, temp_dev)
 
        rhs = rhs_dev
        end select
@@ -49,6 +60,7 @@ contains
     real, intent(out) :: rhs(:, :, :)
     real, intent(inout) :: temp(:, :, :)
     integer :: x, y, z, nx, ny, nz
+    real :: dx
 
     nx = size(data, dim=1)
     ny = size(data, dim=2)
@@ -56,29 +68,87 @@ contains
     y = blockDim%x*(blockIdx%x-1) + threadIdx%x
     z = blockDim%y*(blockIdx%y-1) + threadIdx%y
 
+    dx = 2. * acos(-1.) / (nx - 1)
+
     if ( y <= ny .and. z <= nz ) then
+       ! Construct the RHS and apply Forward Thomas
        do x = 1, nx
           temp(x, y, z) = data(x, y, z)
        end do
-       do x = 1, nx
-          temp(x, y, z) = 2.*temp(x, y, z)
+       ! Backward Thomas
+       do x = nx, 1, -1
+          temp(x, y, z) = -1. * cos((x-1)*dx) * cos((y-1)*dx) * cos((z-1)*dx) !2.*temp(x, y, z)
        end do
+       ! Periodic last pass
        do x = 1, nx
           rhs(x, y, z) = temp(x, y, z)
        end do
     end if
 
    end subroutine rhs_x
-  
-  attributes(global) pure subroutine rhs_kernel(data, rhs)
+
+  attributes(global) pure subroutine rhs_y(data, rhs, temp)
     real, intent(in) :: data(:, :, :)
-    real, intent(out) :: rhs(:, :, :)
-    integer :: i, j, k
+    real, intent(inout) :: rhs(:, :, :)
+    real, intent(inout) :: temp(:, :, :)
+    integer :: x, y, z, nx, ny, nz
+    real :: dx
 
-    i = threadIdx%x
-    j = threadIdx%y
-    k = threadIdx%z
-    rhs(i, j, k) = 2. * data(i, j, k)
-   end subroutine rhs_kernel
+    nx = size(data, dim=1)
+    ny = size(data, dim=2)
+    nz = size(data, dim=3)
+    x = blockDim%x*(blockIdx%x-1) + threadIdx%x
+    z = blockDim%y*(blockIdx%y-1) + threadIdx%y
 
+    dx = 2. * acos(-1.) / (nx - 1)
+
+    if ( x <= nx .and. z <= nz ) then
+       ! Construct the RHS and apply Forward Thomas
+       do y = 1, ny
+          temp(x, y, z) = data(x, y, z)
+       end do
+       ! Backward Thomas
+       do y = ny, 1, -1
+          temp(x, y, z) = -1. * cos((x-1)*dx) * cos((y-1)*dx) * cos((z-1)*dx) !2.*temp(x, y, z)
+       end do
+       ! Periodic last pass
+       do y = 1, ny
+          rhs(x, y, z) = rhs(x, y, z) + temp(x, y, z)
+       end do
+    end if
+
+   end subroutine rhs_y
+
+  attributes(global) pure subroutine rhs_z(data, rhs, temp)
+    real, intent(in) :: data(:, :, :)
+    real, intent(inout) :: rhs(:, :, :)
+    real, intent(inout) :: temp(:, :, :)
+    integer :: x, y, z, nx, ny, nz
+    real :: dx
+
+    nx = size(data, dim=1)
+    ny = size(data, dim=2)
+    nz = size(data, dim=3)
+    x = blockDim%x*(blockIdx%x-1) + threadIdx%x
+    y = blockDim%y*(blockIdx%y-1) + threadIdx%y
+
+    dx = 2. * acos(-1.) / (nx - 1)
+
+    if ( x <= nx .and. y <= ny ) then
+       ! Construct the RHS and apply Forward Thomas
+       do z = 1, nz
+          temp(x, y, z) = data(x, y, z)
+       end do
+       ! Backward Thomas
+       do z = nz, 1, -1
+          temp(x, y, z) = -1. * cos((x-1)*dx) * cos((y-1)*dx) * cos((z-1)*dx) !2.*temp(x, y, z)
+       end do
+       ! Periodic last pass
+       do z = 1, nz
+          rhs(x, y, z) = rhs(x, y, z) + temp(x, y, z)
+       end do
+    end if
+
+   end subroutine rhs_z
+  
 end module field_gpu
